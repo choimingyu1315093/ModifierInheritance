fdbba7befbe6622411dca6ed6ca0ff78d1bad57e
choimingyu <alsrb1315093@gmail.com>
2025-07-24 14:03:53 +0900

    Modifier 전달

--- app/src/main/java/com/example/modifierinheritance/MainActivity.kt
+++ app/src/main/java/com/example/modifierinheritance/MainActivity.kt
@@ -33,61 +33,20 @@ class MainActivity : ComponentActivity() {
                 Spacer(modifier = Modifier.height(10.dp))
                 InfoCard("두번째", modifier = Modifier.shadow(4.dp, RoundedCornerShape(8.dp)).background(Color.White))
                 Spacer(modifier = Modifier.height(10.dp))
-                InfoCard("세번째", modifier = Modifier.shadow(10.dp, RoundedCornerShape(20.dp)).background(Color.Blue))
+                InfoCard("세번째")
             }
         }
     }
 }
 
-//아무런 속성도 넣지 않음
-//@Composable
-//fun InfoCard(title: String){
-//    Column { //상위 컴포저블
-//        Text( //하위 컴포저블
-//            text = title
-//        )
-//    }
-//}
-
-//하위 컴포저블에 속성 추가
-//@Composable
-//fun InfoCard(title: String){
-//    Column {
-//        Text(
-//            modifier = Modifier.background(Color.Red).padding(20.dp),
-//            text = title
-//        )
-//    }
-//}
-
-
-//InfoCard를 다 만들고 사용하려고 하는데, 두번째 InfoCard의 모서리를 둥글게 바꾸고 싶어
-//그러면 InfoCard2 뭐 이런식으로 만들어? 만약 이런게 100개라면?
-//그래서 상위 컴포저블에서 하위 컴포저블로 Modifier를 전달하는게 중요하다. 재사용성이 향상 된다.
-
-
-//Modifier 전달
-//@Composable
-//fun InfoCard(title: String, modifier: Modifier = Modifier){
-//    Column (
-//        modifier = modifier
-//    ){
-//        Text(
-//            modifier = Modifier.background(Color.Red).padding(20.dp),
-//            text = title
-//        )
-//    }
-//}
-
-//이렇게 다 만들고 보니 두번째 InfoCard의 텍스트 배경을 흰색으로 바꾸고 싶어졌다.
 @Composable
 fun InfoCard(title: String, modifier: Modifier = Modifier){
     Column (
         modifier = modifier
     ){
         Text(
-            modifier = Modifier.background(Color.Red).padding(20.dp).then(modifier),
-            text = title
+            text = title,
+            modifier = Modifier.background(Color.Red).padding(20.dp).then(modifier)
         )
     }
 }
